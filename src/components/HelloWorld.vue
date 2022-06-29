f7198e7a7823f382e5d3088ab9ec83f0d2147e9e
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-29 11:33:05 +0900

    사이즈 지정 및 리사이즈 이벤트추가

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,12 @@
 <template>
     <kakao-map></kakao-map>
-    {{arr}}
-  <input type="button" value="change" @click="change">
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import KakaoMap from './KakaoMap.vue';
 export default {
   components: { KakaoMap },
   name: 'HelloWorld',
-   computed: {
-        ...mapGetters({
-            arr: 'getArr'
-        })
-    },
-    methods:{
-      change(){
-        this.$store.dispatch('callApi');
-      }
-    },
 }
 </script>
 
