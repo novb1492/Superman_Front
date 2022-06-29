1f17d614f5ffdf6c03027982b07181ed03a87fe6
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-29 13:41:38 +0900

    좌표 => 주소=>해당지역 마트 표시

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -20,7 +20,10 @@ export default {
     };
     this.$store.dispatch('init',values); //지도 생성
     this.setDomSize(window.innerWidth,window.innerHeight);//돔 사이즈 지정
-     window.addEventListener('resize',this.setDomResize);// 리사이즈 이벤트 지정
+    window.addEventListener('resize',this.setDomResize);// 리사이즈 이벤트 지정
+    window.kakao.maps.event.addListener(this.map, 'center_changed',() =>{//중심점 변경시(드래그)인근 마트들 검색
+    this.searchForKeyword(this.map.getCenter());
+  });
   },
   methods:{
     setDomSize(width,height){
@@ -31,7 +34,30 @@ export default {
     },
     setDomResize(){
       this.setDomSize(window.innerWidth,window.innerHeight);
-    }
+    },
+    searchForKeyword(latlng){
+      console.log(latlng.La);
+      //좌표를 주소로 변환
+      var geocoder = new window.kakao.maps.services.Geocoder();
+      geocoder.coord2Address(latlng.La, latlng.Ma, this.searchMarket);
+    },
+    searchMarket(result, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        var ps = new window.kakao.maps.services.Places(); 
+        var address=result[0].address;
+        ps.keywordSearch(address.region_1depth_name+' '+address.region_2depth_name+' '+address.region_3depth_name+' 마트', this.showMarkets); 
+        ps.keywordSearch(address.region_1depth_name+' '+address.region_2depth_name+' '+address.region_3depth_name+' 슈퍼', this.showMarkets);
+      }   
+    },
+    showMarkets(data, status, pagination){
+      if(status === window.kakao.maps.services.Status.OK){
+        console.log(data);
+        console.log(status);
+        console.log(pagination);
+        return;
+      }
+    },
+    
   }   
 }
 </script>
