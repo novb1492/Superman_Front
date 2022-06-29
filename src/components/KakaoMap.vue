aa24a2a9f3302f7276abcaba407c4d9e2a24650c
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-29 11:34:13 +0900

    dom 제어 ref로 변경

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -13,7 +13,7 @@ export default {
   },
   mounted(){
     var values=new Object;
-    values.container=document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    values.container=this.$refs.map; //지도를 담을 영역의 DOM 레퍼런스
     values.options= { //지도를 생성할 때 필요한 기본 옵션
         center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
         level: 3 //지도의 레벨(확대, 축소 정도)
