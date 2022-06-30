2cdbec22e66ef4e1c8c927b2a62aec652ff51aad
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-30 14:34:20 +0900

    검색 방법 및 중복 push 방지

---
+++ src/components/showMarketInfo.vue
@@ -0,0 +1,47 @@
+<template>
+all
+<br>
+ <swiper
+    :slides-per-view="1.2"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  > 
+ 
+
+<swiper-slide v-for="(item,index) in arr " :key="item">
+{{index}}
+</swiper-slide>
+
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+export default {
+  name: 'showMarketInfo',
+  components: { Swiper,SwiperSlide, },
+  props:['arr'],
+  setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+      };
+  },
+  mounted(){
+    console.log(this.arr);
+  },
+  methods:{
+
+    
+  }   
+}
+</script>
+
+
