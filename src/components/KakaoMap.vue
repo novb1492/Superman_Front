326e53120c3c4e41b270646c200ada7defcb9a50
Enigma <74756147+novb1492@users.noreply.github.com>
2022-06-30 16:47:24 +0900

    info창이랑 겹친거였음

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -56,15 +56,7 @@ export default {
       var x=item.x;
       var y=item.y;
       this.map.panTo(new window.kakao.maps.LatLng(y, x));
-      var iwContent = '<div style="padding:5px;"><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">'+item.place_name+'</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      iwPosition = new window.kakao.maps.LatLng(y, x); //인포윈도우 표시 위치입니다
-      // 인포윈도우를 생성합니다
-      var infowindow = new window.kakao.maps.InfoWindow({
-          position : iwPosition, 
-          content : iwContent 
-      });
-      // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-      infowindow.open(this.map);
+
     },  
     
   }   
